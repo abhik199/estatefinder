ca03db32b3e077aa31d5951010450a1326a1bd6b
abhik199 <abhishekkirar2004@gmail.com>
2024-07-21 01:13:16 +0530

    ejs add

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
-
-const connect = require("./config/db.connection");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(express.static(path.join(__dirname, "../public")));
 
-const port = process.env.PORT || 3200;
+app.use("/", require("./routes/routes"));
 
-connect()
-  .then((res) => {
-    app.listen(port, function () {
-      console.log(`Server is running at ${port}`);
-    });
-  })
-  .catch((err) => {
-    console.error("Failed to connect to the database", err);
-  });
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const app = require("./app");
+const connect = require("./config/db.connection");
+const port = process.env.PORT || 3200;
+
+// connect()
+//   .then((res) => {
+//     app.listen(port, function () {
+//       console.log(`Server is running at ${port}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.error("Failed to connect to the database", err);
+//   });
+
+app.listen(port, function () {
+  console.log(`Server is running at ${port}`);
+});
