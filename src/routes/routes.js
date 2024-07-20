29a36b3a08dcf72ee9159c2e36e71871bea1ece8
abhik199 <abhishekkirar2004@gmail.com>
2024-07-21 05:05:26 +0530

    add

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,5 +3,12 @@ const routes = require("express").Router();
 routes.get("/", (req, res) => {
   res.render("index");
 });
+routes.get("/about", (req, res) => {
+  res.render("about");
+});
+
+routes.get("/property", (req, res) => {
+  res.render("property");
+});
 
 module.exports = routes;
